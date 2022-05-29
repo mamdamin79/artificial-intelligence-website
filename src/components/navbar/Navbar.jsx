faed7a4e8d6f7e6d62fafcf2dfce2d3e1e4721a7
mohammadaminsaheb <mohammadaminsaheb@gmail.com>
2022-05-29 12:52:16 -0700

    add assets and creating navbar for all sizes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 // styles
 import styles from './navbar.module.css';
+// logo
+import logo from '../../assets/images/GPT-3.png';
+// icons
+import { BiCar, BiMenu, BiX } from "react-icons/bi";
 const Navbar = () => {
+    const [show, setShow] = useState(false)
     return (
-        <div>
-            Navbar
+        <div className={styles.navbarContainer}>
+            <div className={styles.logoContainer}>
+                <img src={logo} alt="logo" />
+            </div>
+            <nav className={styles.nav}>
+                <a href="">
+                    Home
+                </a>
+                <a href="">
+                    what is GPT?
+                </a>
+                <a href="">
+                    Open AI
+                </a>
+                <a href="">
+                    Case Studies
+                </a>
+                <a href="">
+                    Library
+                </a>
+            </nav>
+            <div className={styles.buttonContainer}>
+                <a href="" className={styles.signup}>sign in</a>
+                <button className={styles.signin}>sign up</button>
+                <button onClick={() => setShow(!show)} className={styles.menuButton}>{show ? <BiX/> : <BiMenu/>}</button>
+            </div>
+            {
+                show && <nav className={styles.mobileMenuContainer}>
+                <a href="">
+                    Home
+                </a>
+                <a href="">
+                    what is GPT?
+                </a>
+                <a href="">
+                    Open AI
+                </a>
+                <a href="">
+                    Case Studies
+                </a>
+                <a href="">
+                    Library
+                </a>
+                <a href="" className={styles.signup}>sign in</a>
+                <button className={styles.signin}>sign up</button>
+            </nav>
+            }
         </div>
     );
 }

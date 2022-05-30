130a2c09b7ec24704884c3c2ec7dc724390e549b
mohammadaminsaheb <mohammadaminsaheb@gmail.com>
2022-05-30 01:17:14 -0700

    creating header section and make it responsive

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 // styles
 import styles from './header.module.css'
+// images
+import people from "../../assets/images/Group 81.png"
+import ai from "../../assets/images/Illustration.png"
 const Header = () => {
     return (
-        <div>
-            Header
-        </div>
+        <header className={styles.header}>
+            <div className={styles.content}>
+                <h1>
+                    Let's Build Something
+                    amazing with GPT-3
+                    OpenAI
+                </h1>
+                <p className={styles.description}>
+                    Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.
+                </p>
+                <div className={styles.formContainer}>
+                    <input type="text" />
+                    <button>Get Start</button>
+                </div>
+                <div className={styles.peopleContainer}>
+                    <img src={people} alt="people" />
+                    <p>1,600 people requested access a visit in last 24 hours</p>
+                </div>
+            </div>
+            <div className={styles.imageContainer}>
+                <img src={ai} alt="ai" />
+            </div>
+        </header>
     );
 }
  

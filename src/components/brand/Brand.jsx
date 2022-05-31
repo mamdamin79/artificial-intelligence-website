10e3a4520d9c70cfcd52a2b9d40627d74c4f4521
mohammadaminsaheb <mohammadaminsaheb@gmail.com>
2022-05-31 09:11:19 -0700

    create brand section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 // styles
 import styles from './brand.module.css';
+// images
+import google from "../../assets/images/google.png";
+import atlassian from "../../assets/images/atlassian.png";
+import dropbox from "../../assets/images/dropbox.png";
+import slack from "../../assets/images/slack.png";
+import shopify from "../../assets/images/shopify.png";
+
 const Brand = () => {
     return (
-        <div>
-            Brand
+        <div className={styles.brandContainer}>
+            <div>
+                <img src={google} alt="google" />
+            </div>
+            <div>
+                <img src={atlassian} alt="atlassian" />
+            </div>
+            <div>
+                <img src={dropbox} alt="dropbox" />
+            </div>
+            <div>
+                <img src={slack} alt="slack" />
+            </div>
+            <div>
+                <img src={shopify} alt="shopify" />
+            </div>
         </div>
     );
 }
